0a7b578be03bcbc289c5ada8a28369b5c6cfd8bd
mustafa yavuz <mustafa.yvz.bote@gmail.com>
2024-09-23 19:40:21 +0300

    balance sheets api

--- controllers/api/balancesheets.js
+++ controllers/api/balancesheets.js
@@ -5,29 +5,25 @@ exports.get = async function(req, res, next) {
         const stockCode = req.query.stockCode
         const period = req.query.period
         if(stockCode && period) {
-            const balanceSheets = await config.BalanceSheet.find({
+            const balanceSheets = await config.BalanceSheet.findOne({
                 stockCode,
                 'balanceSheets.period': period
             })
             res.status(200).json({data: balanceSheets})
-            console.log("Koşul 1")
             return
         }
         if(stockCode) {
-            const balanceSheets = await config.BalanceSheet.find({ stockCode })
+            const balanceSheets = await config.BalanceSheet.findOne({ stockCode })
             res.status(200).json({data: balanceSheets})
-            console.log("Koşul 2")
             return
         }
         if(period) {
             const balanceSheets = await config.BalanceSheet.find({ 'balanceSheets.period': period })
             res.status(200).json({data: balanceSheets})
-            console.log("Koşul 3")
             return
         }
         const balanceSheets = await config.BalanceSheet.find()
         res.status(200).json({data: balanceSheets})
-        console.log("Koşulsuz")
     } catch (error) {
         res.status(500).json({error: {message: "Bilinmeyen bir hata oluştu", detail: error}})
     }
