cd55f99a35ab599b58ffc4ed175afefc6d49c99c
mustafa yavuz <mustafa.yvz.bote@gmail.com>
2024-10-19 12:37:12 +0300

    create index.html

--- app.js
+++ app.js
@@ -1,46 +1,50 @@
-const express = require("express");
-const cron = require('node-cron');
+const express = require("express")
+const cron = require('node-cron')
 
-const db = require("./config/db");
-const notificationUtil = require('./utils/notification');
-const balanceSheetUtil = require('./utils/balancesheet');
-const dateUtil = require('./utils/date');
+const db = require("./config/db")
+const notificationUtil = require('./utils/notification')
+const balanceSheetUtil = require('./utils/balancesheet')
+const dateUtil = require('./utils/date')
 
-const app = express();
+const app = express()
 
-app.use(express.json());
+app.use(express.json())
+app.use('/bootstraplib', express.static('node_modules/bootstrap'))
+app.use('/static', express.static('public'))
+
+app.set('view engine', 'ejs')
 
 // Yönlendirme ayarları
-require('./routes/manager')(app);
+require('./routes/manager')(app)
 
 // Cron job ile her gece çalışacak görev
 const schedule = cron.schedule('0 0 * * *', async () => {
-    await notificationUtil.memberDisclosureQuery();
-    await balanceSheetUtil.getBalanceSheets(`${dateUtil.nowYear() - 1}`);
-    await balanceSheetUtil.getBalanceSheets(`${dateUtil.nowYear()}`);
-});
+    await notificationUtil.memberDisclosureQuery()
+    await balanceSheetUtil.getBalanceSheets(`${dateUtil.nowYear() - 1}`)
+    await balanceSheetUtil.getBalanceSheets(`${dateUtil.nowYear()}`)
+})
 
 // Sunucu dinleme ve veritabanı bağlantısı
 app.listen(3001, async () => {
     try {
-        await db.connectDB(); // Veritabanı bağlantısı kurulduktan sonra sunucu başlar
-        console.log("Sunucu 3001 portunda dinleniyor");
-        schedule.start(); // Veritabanı bağlantısı başarılıysa cron işini başlat
+        await db.connectDB() // Veritabanı bağlantısı kurulduktan sonra sunucu başlar
+        console.log("Sunucu 3001 portunda dinleniyor")
+        schedule.start() // Veritabanı bağlantısı başarılıysa cron işini başlat
     } catch (err) {
-        console.error("Veritabanına bağlanırken bir hata oluştu:", err);
-        process.exit(1); // Bağlantı başarısızsa sunucu başlatılmaz
+        console.error("Veritabanına bağlanırken bir hata oluştu:", err)
+        process.exit(1) // Bağlantı başarısızsa sunucu başlatılmaz
     }
-});
+})
 
 // Sunucu kapanış sinyallerini yakalama ve veritabanı bağlantısını kapatma
 process.on('SIGINT', async () => {
-    console.log("SIGINT sinyali alındı. Sunucu kapatılıyor...");
-    await db.closeDB(); // Veritabanı bağlantısını düzgün kapat
-    process.exit(0);
-});
+    console.log("SIGINT sinyali alındı. Sunucu kapatılıyor...")
+    await db.closeDB() // Veritabanı bağlantısını düzgün kapat
+    process.exit(0)
+})
 
 process.on('SIGTERM', async () => {
-    console.log("SIGTERM sinyali alındı. Sunucu kapatılıyor...");
-    await db.closeDB(); // Veritabanı bağlantısını düzgün kapat
-    process.exit(0);
-});
+    console.log("SIGTERM sinyali alındı. Sunucu kapatılıyor...")
+    await db.closeDB() // Veritabanı bağlantısını düzgün kapat
+    process.exit(0)
+})
--- controllers/indexPage.js
+++ controllers/indexPage.js
@@ -1,9 +1,10 @@
 const notificationUtil = require('../utils/notification')
+const path = require('path')
 
 exports.get = async function (req, res, next) {
     try {
-        await notificationUtil.memberDisclosureQuery()
-        res.status(200).json({ message: "BORSA ANALIZIM" })
+        // await notificationUtil.memberDisclosureQuery()
+        res.sendFile(path.join(__dirname + '/../views', 'index.html'))
     } catch (error) {
         res.status(500).json({ error: { message: "Bilinmeyen bir hata oluştu", detail: error } })
     }
