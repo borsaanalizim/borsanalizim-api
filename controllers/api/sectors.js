1710aaa48071d4e752ccc76da3a180ea0e1262bf
mustafa yavuz <mustafa.yvz.bote@gmail.com>
2024-11-05 19:24:58 +0300

    fixed sectors api

--- controllers/api/sectors.js
+++ controllers/api/sectors.js
@@ -2,8 +2,12 @@ const config = require("../../config/config")
 
 exports.get = async function(req, res, next) {
     try {
-        const sectors = await config.Sector.find();
-        res.status(200).json({data: sectors})
+        const sectors = await config.Sector.findOne()
+        if (sectors) {
+            res.status(200).json({ data: sectors })
+        } else {
+            res.status(404).json({ error: { message: "Sektör verisi bulunamadı" } })
+        }
     } catch (error) {
         res.status(500).json({error: {message: "Bilinmeyen bir hata oluştu", detail: error}})
     }
