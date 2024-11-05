b17bf797bd74a8fdab09afee152f36bdec697199
mustafa yavuz <mustafa.yvz.bote@gmail.com>
2024-11-05 21:15:56 +0300

    refactored balancesheetdates

--- controllers/balancesheetdates.js
+++ controllers/balancesheetdates.js
@@ -1,6 +1,7 @@
 const notificationUtil = require('../utils/notification')
+const config = require('../config/config')
 
-exports.get = async function (req, res, next) {
+async function addBalanceSheetDates(req, res, next) {
     try {
         const mkkMemberOid = req.query.mkkMemberOid
         notificationUtil.memberDisclosureQuery(mkkMemberOid)
@@ -8,4 +9,18 @@ exports.get = async function (req, res, next) {
     } catch (error) {
         res.status(500).json({ error: { message: "Bilinmeyen bir hata oluştu", detail: error } })
     }
+}
+
+async function deleteAllBalanceSheetDates(req, res, next) {
+    try {
+        await config.BalanceSheetDate.deleteMany({})
+        res.status(200).send("Kayıtlar silindi")
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('Hata oluştu:' + error) // Hata mesajını daha kullanıcı dostu hale getirin
+    }
+}
+
+exports.get = async function (req, res, next) {
+    await addBalanceSheetDates(req, res, next)
 }
